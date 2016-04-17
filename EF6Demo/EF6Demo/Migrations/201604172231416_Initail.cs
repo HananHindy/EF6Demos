24f744d76b7f67c47b4e9d003da556833e634b5a
Hanan Hendy <Hanan.hendy@gmail.com>
2016-04-18 00:39:04 +0200

    Add initial migration

---
+++ EF6Demo/EF6Demo/Migrations/201604172231416_Initail.cs
@@ -0,0 +1,37 @@
+namespace EF6Demo.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initail : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Classes",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        Title = c.String(),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            CreateTable(
+                "dbo.Students",
+                c => new
+                    {
+                        StudentID = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastName = c.String(),
+                    })
+                .PrimaryKey(t => t.StudentID);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Students");
+            DropTable("dbo.Classes");
+        }
+    }
+}
