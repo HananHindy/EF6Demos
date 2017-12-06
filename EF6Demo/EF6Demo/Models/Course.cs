f7504d5fc19778c89be3bb175c6bc6deac283a23
HananHindy <hanan.hendy@gmail.com>
2017-12-06 11:37:58 +0200

    Update Course.cs
    Add prop ID2

--- EF6Demo/EF6Demo/Models/Course.cs
+++ EF6Demo/EF6Demo/Models/Course.cs
@@ -8,6 +8,7 @@ namespace EF6Demo.Models
     public class Course
     {
         public int ID { get; set; }
+        public int ID2 { get; set; }
 
         public string CourseTitle { get; set; }
         public List<Student2> Students { get; set; }
